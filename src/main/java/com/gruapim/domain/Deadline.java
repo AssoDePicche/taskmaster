ea79333087c5c64e87a1dc47d79956af0b12ffe2
AssoDePicche <samuelprado730@gmail.com>
2025-10-03 17:17:04 -0300

    feat: override 'toString' method in 'Deadline' class

--- src/main/java/com/gruapim/domain/Deadline.java
+++ src/main/java/com/gruapim/domain/Deadline.java
@@ -1,8 +1,11 @@
 package com.gruapim.domain;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record Deadline(LocalDateTime value) {
+  private static DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
   public Deadline {
     if (!value.isAfter(LocalDateTime.now())) {
       throw new IllegalArgumentException("A data limite deve ser futura");
@@ -11,5 +14,10 @@ public record Deadline(LocalDateTime value) {
 
   public LocalDateTime toLocalDateTime() {
     return value;
+  }
+
+  @Override
+  public String toString() {
+    return value.format(formatter);
   }
 }
