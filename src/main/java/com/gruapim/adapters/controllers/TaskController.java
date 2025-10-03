72e695505ec1bb161ad09a0cc2704816cdb7f298
AssoDePicche <samuelprado730@gmail.com>
2025-10-03 17:20:23 -0300

    refactor: move 'TaskMapper' from 'TaskService' to 'TaskController' + add 'PostMapping' route and OpenAPI docs

--- src/main/java/com/gruapim/adapters/controllers/TaskController.java
+++ src/main/java/com/gruapim/adapters/controllers/TaskController.java
@@ -5,16 +5,28 @@ import com.gruapim.application.dto.TaskPatch;
 import com.gruapim.application.dto.TaskRequest;
 import com.gruapim.application.dto.TaskResponse;
 import com.gruapim.application.services.TaskService;
+import com.gruapim.domain.Category;
+import com.gruapim.infrastructure.mappers.TaskMapper;
+import com.gruapim.infrastructure.persistence.entities.TaskEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,51 +34,97 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tasks")
+@Tag(description = "API para gerenciamento de tarefas", name = "Tarefas")
+@Validated
 public class TaskController {
+  private final TaskMapper mapper;
+
   private final TaskService service;
 
-  public TaskController(TaskService service) {
+  public TaskController(TaskMapper mapper, TaskService service) {
+    this.mapper = mapper;
+
     this.service = service;
   }
 
   @DeleteMapping("/{id}")
+  @Operation(description = "Remove permanentemente uma tarefa", summary = "Excluir tarefa",
+      responses =
+      {
+        @ApiResponse(description = "Tarefa excluída com sucesso", responseCode = "204")
+        , @ApiResponse(description = "Tarefa não encontrada", responseCode = "404")
+      })
   public ResponseEntity<Void> delete(@PathVariable Long id) throws Exception {
-    TaskResponse task = queryOrThrowNotFound(id);
+    TaskEntity task = service.query(id).orElseThrow(() -> new NotFoundException());
 
-    service.delete(task.id());
+    service.delete(task.getId());
 
     return ResponseEntity.noContent().build();
   }
 
   @GetMapping
-  public ResponseEntity<Page<TaskResponse>> get(@RequestParam(defaultValue = "0") int page,
-      @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "id") String sort) {
-    Page<TaskResponse> response = service.query(page, size, sort);
+  @Operation(
+      description = "Retorna uma lista paginada de todas as tarefas ou filtradas por categoria",
+      summary = "Listar tarefas",
+      responses =
+      { @ApiResponse(description = "Tarefas listadas com sucesso", responseCode = "200") })
+  public ResponseEntity<Page<TaskResponse>>
+  get(@Parameter(description = "Filtra tarefas por categoria (opcional)", name = "categoria",
+          required = false) @RequestParam(name = "categoria", required = false) Category category,
+      @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+    Page<TaskEntity> entities =
+        (null != category) ? service.query(category, pageable) : service.query(pageable);
+
+    Page<TaskResponse> response = entities.map(mapper::map);
 
     return ResponseEntity.ok().body(response);
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<TaskResponse> get(@PathVariable Long id) throws Exception {
-    TaskResponse response = queryOrThrowNotFound(id);
+  @Operation(description = "Retorna os detalhes de uma tarefa", summary = "Buscar tarefa por id",
+      responses =
+      {
+        @ApiResponse(description = "Tarefa encontrada com sucesso", responseCode = "200")
+        , @ApiResponse(description = "Tarefa não encontrada", responseCode = "404")
+      })
+  public ResponseEntity<TaskResponse>
+  get(@PathVariable Long id) throws Exception {
+    TaskEntity entity = service.query(id).orElseThrow(() -> new NotFoundException());
+
+    TaskResponse response = mapper.map(entity);
 
     return ResponseEntity.ok().body(response);
   }
 
   @PatchMapping("/{id}")
-  public ResponseEntity<TaskResponse> patch(
-      @PathVariable Long id, @Valid @RequestBody TaskPatch request) throws Exception {
-    TaskResponse task = queryOrThrowNotFound(id);
+  @Operation(description = "Atualiza parcialmente os detalhes de uma tarefa",
+      summary = "Atualizar parcialmente uma tarefa",
+      responses =
+      {
+        @ApiResponse(description = "Tarefa atualizada com sucesso", responseCode = "200")
+        , @ApiResponse(description = "Tarefa não encontrada", responseCode = "404")
+      })
+  public ResponseEntity<TaskResponse>
+  patch(@PathVariable Long id, @Valid @RequestBody TaskPatch request) throws Exception {
+    TaskEntity entity = service.query(id).orElseThrow(() -> new NotFoundException());
 
-    TaskResponse response = service.update(task, request);
+    TaskResponse response = mapper.map(service.update(entity, request));
 
     return ResponseEntity.ok().body(response);
   }
 
   @PostMapping
-  public ResponseEntity<TaskResponse> post(@Valid @RequestBody TaskRequest request)
-      throws Exception {
-    TaskResponse response = service.save(request);
+  @Operation(description = "Cria uma tarefa", summary = "Criar tarefa",
+      responses =
+      {
+        @ApiResponse(description = "Tarefa criada com sucesso", responseCode = "201")
+        , @ApiResponse(description = "Dados inválidos", responseCode = "400")
+      })
+  public ResponseEntity<TaskResponse>
+  post(@Valid @RequestBody TaskRequest request) throws Exception {
+    TaskEntity entity = mapper.map(request);
+
+    TaskResponse response = mapper.map(service.save(entity));
 
     String path = String.format("/tasks/%d", response.id());
 
@@ -75,13 +133,19 @@ public class TaskController {
     return ResponseEntity.created(uri).body(response);
   }
 
-  private TaskResponse queryOrThrowNotFound(Long id) throws Exception {
-    Optional<TaskResponse> task = service.query(id);
+  @PutMapping("/{id}")
+  @Operation(description = "Atualiza todos os detalhes de uma tarefa", summary = "Atualizar tarefa",
+      responses =
+      {
+        @ApiResponse(description = "Tarefa atualizada com sucesso", responseCode = "200")
+        , @ApiResponse(description = "Tarefa não encontrada", responseCode = "404")
+      })
+  public ResponseEntity<TaskResponse>
+  put(@PathVariable Long id, @Valid @RequestBody TaskRequest request) throws Exception {
+    TaskEntity entity = service.query(id).orElseThrow(() -> new NotFoundException());
 
-    if (!task.isPresent()) {
-      throw new NotFoundException();
-    }
+    TaskResponse response = mapper.map(service.update(entity, request));
 
-    return task.get();
+    return ResponseEntity.ok().body(response);
   }
 }
--- src/main/java/com/gruapim/application/services/TaskService.java
+++ src/main/java/com/gruapim/application/services/TaskService.java
@@ -2,64 +2,74 @@ package com.gruapim.application.services;
 
 import com.gruapim.application.dto.TaskPatch;
 import com.gruapim.application.dto.TaskRequest;
-import com.gruapim.application.dto.TaskResponse;
-import com.gruapim.infrastructure.mappers.TaskMapper;
+import com.gruapim.domain.Category;
 import com.gruapim.infrastructure.persistence.entities.TaskEntity;
 import com.gruapim.infrastructure.persistence.repositories.TaskJpaRepository;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TaskService {
   private final TaskJpaRepository repository;
 
-  private final TaskMapper mapper;
-
-  public TaskService(TaskJpaRepository repository, TaskMapper mapper) {
+  public TaskService(TaskJpaRepository repository) {
     this.repository = repository;
-
-    this.mapper = mapper;
   }
 
+  @Transactional
   public void delete(Long id) {
     repository.deleteById(id);
   }
 
-  public Optional<TaskResponse> query(Long id) {
-    return repository.findById(id).map((task) -> mapper.map(task));
+  public Optional<TaskEntity> query(Long id) {
+    return repository.findById(id);
   }
 
-  public Page<TaskResponse> query(int page, int size, String sort) {
-    Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
-
-    return repository.findAll(pageable).map((task) -> mapper.map(task));
+  public Page<TaskEntity> query(Pageable pageable) {
+    return repository.findAll(pageable);
   }
 
-  public TaskResponse save(TaskRequest request) {
-    TaskEntity task = mapper.map(request);
+  public Page<TaskEntity> query(Category category, Pageable pageable) {
+    return repository.findAllByCategory_Value(category.toString(), pageable);
+  }
+
+  @Transactional
+  public TaskEntity save(TaskEntity task) {
+    return repository.save(task);
+  }
+
+  @Transactional
+  public TaskEntity update(TaskEntity outdated, TaskPatch request) {
+    TaskEntity task = new TaskEntity(outdated.getId(),
+        Objects.requireNonNullElse(request.title(), outdated.getTitle().toString()),
+        Objects.requireNonNullElse(request.category(), outdated.getCategory().toString()),
+        Objects.requireNonNullElse(request.description(), outdated.getDescription().toString()),
+        Objects.requireNonNullElse(request.priority(), outdated.getPriority().toInteger()),
+        Objects.requireNonNullElse(
+            request.deadline(), LocalDateTime.parse(outdated.getDeadline().toString())),
+        Objects.requireNonNullElse(request.done(), outdated.isDone()),
+        LocalDateTime.parse(outdated.getCreatedAt().toString()),
+        LocalDateTime.parse(outdated.getUpdatedAt().toString()));
 
     repository.save(task);
 
-    return mapper.map(task);
+    return task;
   }
 
-  public TaskResponse update(TaskResponse outdated, TaskPatch request) {
-    TaskEntity task = new TaskEntity(outdated.id(),
-        Objects.requireNonNullElse(request.title(), outdated.title()),
-        Objects.requireNonNullElse(request.category(), outdated.category()),
-        Objects.requireNonNullElse(request.description(), outdated.description()),
-        Objects.requireNonNullElse(request.priority(), outdated.priority()),
-        Objects.requireNonNullElse(request.deadline(), LocalDateTime.parse(outdated.deadline())),
-        LocalDateTime.parse(outdated.createdAt()), LocalDateTime.parse(outdated.updatedAt()));
+  @Transactional
+  public TaskEntity update(TaskEntity outdated, TaskRequest request) {
+    TaskEntity task = new TaskEntity(outdated.getId(), request.title(), request.category(),
+        request.description(), request.priority(), request.deadline(), outdated.isDone(),
+        LocalDateTime.parse(outdated.getCreatedAt().toString()),
+        LocalDateTime.parse(outdated.getUpdatedAt().toString()));
 
     repository.save(task);
 
-    return mapper.map(task);
+    return task;
   }
 }
