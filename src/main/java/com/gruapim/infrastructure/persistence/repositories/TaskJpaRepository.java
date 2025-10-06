96fee98f70cf6e18ed12091e4580b7f765a96841
AssoDePicche <samuelprado730@gmail.com>
2025-10-06 00:55:46 -0300

    feat: add repository annotation

--- src/main/java/com/gruapim/infrastructure/persistence/repositories/TaskJpaRepository.java
+++ src/main/java/com/gruapim/infrastructure/persistence/repositories/TaskJpaRepository.java
@@ -4,7 +4,9 @@ import com.gruapim.infrastructure.persistence.entities.TaskEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskJpaRepository extends JpaRepository<TaskEntity, Long> {
   Page<TaskEntity> findAll(Pageable pageable);
 
